91576b9f0290828ddfd73cc90b78fc4ab19dcae1
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-05-26 09:28:37 +0200

    v1

--- scriptsSQL-Server/consultasCaminoIgnacinoJuanDiego.sql
+++ scriptsSQL-Server/consultasCaminoIgnacinoJuanDiego.sql
@@ -6,11 +6,13 @@ select Alojamiento.nombre, Poblacion.nombre from Alojamiento inner join Poblacio
 /*Ver los puntos de interes de cada población*/
 select nombre, nombrePtoInteres from PuntoInteres inner join Poblacion on PuntoInteres.idPoblacion = Poblacion.idPoblacion;
 
-/*Sacar por que poblaciones pasa cada etapa*/
+/*Sacar por que población de inicio y que población de fin pasa cada etapa*/
+select idEtapa, PI.nombre as 'Poblacion Inicio', PF.nombre as 'Poblacion Fin' from Etapa inner join Poblacion PI on Etapa.idPoblacionInicio = PI.idPoblacion inner join Poblacion PF on Etapa.idPoblacionFin = PF.idPoblacion;
+
 
 /* Ver que actividad se realiza en cada etapa*/
 select nombre, idEtapa from Actividad;
 
-/*Saber que poblaciones se repiten en el punto de intereés*/
+/*Saber que poblaciones se repiten en el punto de interés*/
 select nombre, count(PuntoInteres.idPoblacion) AS 'Poblaciones que se repiten en el Punto de Interes'  from PuntoInteres inner join Poblacion on PuntoInteres.idPoblacion = Poblacion.idPoblacion
 group by nombre;
